3d6ed5b181c7ddc66f57f5395d16371b09093b07
luizfper <luizfper@gmail.com>
2019-06-12 22:13:32 -0400

    kind of working

--- app/controllers/requestcontroller.rb
+++ app/controllers/requestcontroller.rb
@@ -10,28 +10,27 @@ class RequestsController < AppController
 	end
 
 	post '/:username/requests/new' do
-			@user = current_user
-			@request = Request.create #({:date_updated => Time.now})
-			@user.requests << @request
+			user = current_user
+			request = Request.create #({:date_updated => Time.now})
+			user.requests << request
 			params[:job_titles].each do |job_title|
 				request.job_titles << JobTitle.create(:job_title => job_title)
 			end
-			@request.locations << Location.create({:city => "Toronto", :province => "Ontario", :country => "Canada"})
-			@user.save
-			@request.save
-			# binding.pry
-			# redirect "/#{@user.username}/requests/new/#{@request.id}/step2"
+			request.locations << Location.create({:city => "Toronto", :province => "Ontario", :country => "Canada"})
+			user.save
+			request.save
+			redirect to ("/#{user.username}/requests/new/#{request.id}/step2")
 	end
 
-	get '/:username/requests/new/:cabola/step2' do
-		binding.pry
+	get '/:username/requests/new/:request/step2' do
 		@user = current_user
 		@request = Request.find(params[:request])
+		binding.pry
 		erb :request_new_step2
 	end
 
-	post '/:username/requests/new/:request/include' do
+	# post '/:username/requests/new/:request/include' do
 
-	end
+	# end
 
 end
